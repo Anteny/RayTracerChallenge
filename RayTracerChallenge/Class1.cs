08c8a6c1742bdc4af83b1df950af38f4e372c844
Anteny <46804777+Anteny@users.noreply.github.com>
2023-10-24 16:21:04 -0700

    Adding initial files
    Started program in visual studio.

---
+++ RayTracerChallenge/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracerChallenge
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static (float x, float y, float z, float w) CreateVectorTuple((float x, float y, float z) vector)
+        // Returns the original vector with an additional element to identify it as a vector
+        {
+            return (vector.x, vector.y, vector.z, 0);
+        }
+
+        public static (float x, float y, float z, float w) CreatePointTuple((float x, float y, float z) point)
+        // Returns the original point with an additional element to identify it as a point
+        {
+            return (point.x, point.y, point.z, 1);
+        }
+
+        public static string IdentifyTuple((float x, float y, float z, float w) ToIdentify)
+        // When given a tuple will return a string stating what the tuple is.
+        {
+            if (ToIdentify.w == 0)
+            {
+                return "vector";
+            }
+            else if (ToIdentify.w == 1)
+            {
+                return "point";
+            }
+        }
+
+        public static bool CompareTuple((float x, float y, float z, float w) TupleA,(float x, float y, float z, float w) TupleB)
+        // Compares two tuples. If they are within .00001 on all elements returns true, otherwise returns false.
+        {
+            if (Math.Abs(TupleA.x - TupleB.x) > 0.00001)
+            {
+                return false;
+            }
+            else if (Math.Abs(TupleA.y - TupleB.y) > 0.00001)
+            {
+                return false;
+            }
+            else if (Math.Abs(TupleA.z - TupleB.z) > 0.00001)
+            {
+                return false;
+            }
+            else if (Math.Abs(TupleA.w - TupleB.w) > 0.00001)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ RayTracerChallengeTests/MainClassTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RayTracerChallenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RayTracerChallenge.Tests
+{
+    [TestClass()]
+    public class MainClassTests
+    {
+        [TestMethod]
+        public void IdentifyTupleVector_Test()
+        {
+            (float x, float y, float z, float w) tuple = ((float)4.3, (float)-4.2, (float)3.1, (float)0.0);
+            string Expected = "vector";
+
+            var Actual = MainClass.IdentifyTuple(tuple);
+
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void IdentifyTuplePoint_Test()
+        {
+            (float x, float y, float z, float w) tuple = ((float)4.3, (float)-4.2, (float)3.1, (float)1.0);
+            string Expected = "point";
+
+            var Actual = MainClass.IdentifyTuple(tuple);
+
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void CreatePointTuple_Test()
+        {
+            (float x, float y, float z) point = (4, -4, 3);
+            float Expected = 1;
+
+            var NewPoint = MainClass.CreatePointTuple(point);
+            float Actual = NewPoint.w;
+
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void CreateVectorTuple_Test()
+        {
+            (float x, float y, float z) vector = (4, -4, 3);
+            float Expected = 0;
+
+            var NewVector = MainClass.CreateVectorTuple(vector);
+            float Actual = NewVector.w;
+
+            Assert.AreEqual(Expected, Actual);
+        }
+
+        [TestMethod]
+        public void AddTuples_Test()
+        {
+            (float x, float y, float z, float w) tuple1 = ((float)3, (float)-2, (float)5, (float)1);
+            (float x, float y, float z, float w) tuple2 = ((float)-2, (float)3, (float)1, (float)0);
+            (float x, float y, float z, float w) Expected = ((float)-2, (float)3, (float)1, (float)0);
+
+            (float x, float y, float z, float w)  Actual = AddTuples(tuple1, tuple2);
+
+            Assert.AreEqual(Expected, Actual);
+        }
+    }
+}
